24908c449b02e55743788ef67a24248fdf19c989
B3nac <Rockapparatus@yahoo.com>
2015-09-12 02:28:10 -0700

    Put classes in headers for easy imports

---
+++ goblin.h
@@ -0,0 +1,11 @@
+/*enemies*/
+
+class goblin
+{
+
+public:
+	int health = 10;
+	static const int attack = 2;
+	static const int defense = 2;
+
+} goblin;
---
+++ player.h
@@ -0,0 +1,15 @@
+/*player*/
+
+class player
+{
+
+public:
+	char name[20];
+	int level = 1;
+	int health = 10;
+	int player_exp = 0;
+	int attack = 1;
+	int defense = 5;
+	int gobchance = 0;
+
+} player;
